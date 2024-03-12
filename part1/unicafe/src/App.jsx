99003771dcc6eff4a8648ab761c1374a4d24195f
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-12 14:24:15 +0100

    feat: Add solution for exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,10 +5,34 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const goodClickHandler = () => setGood(good + 1);
-  const neutralClickHandler = () => setNeutral(neutral + 1);
-  const badClickHandler = () => setBad(bad + 1);
+  const goodClickHandler = () => {
+    const newGood = good + 1;
+    setGood(newGood);
+    const newAll = newGood + neutral + bad;
+    setAll(newAll);
+    setAverage((newGood - bad) / newAll);
+    setPositive((newGood / newAll) * 100);
+  };
+  const neutralClickHandler = () => {
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+    const newAll = good + newNeutral + bad;
+    setAll(newAll);
+    setAverage((good - bad) / newAll);
+    setPositive((good / newAll) * 100);
+  };
+  const badClickHandler = () => {
+    const newBad = bad + 1;
+    setBad(newBad);
+    const newAll = good + neutral + newBad;
+    setAll(newAll);
+    setAverage((good - newBad) / newAll);
+    setPositive((good / newAll) * 100);
+  };
 
   return (
     <div>
@@ -20,6 +44,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
