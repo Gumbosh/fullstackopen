a47b51229e6a43211cfe8d528c60cf0e43bc2811
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-12 13:48:42 +0100

    fix: Add properly solution for exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
+import Button from "./components/Button";
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const goodClickHandler = () => setGood(good + 1);
+  const neutralClickHandler = () => setNeutral(neutral + 1);
+  const badClickHandler = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give Feedback</h1>
+      <Button onClick={goodClickHandler} text="good" />
+      <Button onClick={neutralClickHandler} text="neutral" />
+      <Button onClick={badClickHandler} text="bad" />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
 };
 
 export default App;
