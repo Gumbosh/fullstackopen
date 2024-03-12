822a6b83f336b25205f04b9191919c2ec09f94bf
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-12 15:38:23 +0100

    feat: Add solution for exercise 1.11

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -8,14 +8,14 @@ const Statistics = ({ ratings }) => {
   return all === 0 ? (
     <p>No feedback given</p>
   ) : (
-    <>
+    <table>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={all} />
       <StatisticLine text="average" value={average} />
       <StatisticLine text="positive" value={`${positive}%`} />
-    </>
+    </table>
   );
 };
 
