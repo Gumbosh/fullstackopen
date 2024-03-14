2830c188109c39dd6ed1e0c5fa83860f6e88d7d5
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-14 16:06:23 +0100

    feat: Add solution for exercise 2.9

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [searchName, setSearchName] = useState("");
   const [newName, setNewName] = useState("");
-  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
   };
-  const handleNewPhoneNumberChange = (event) => {
-    setNewPhoneNumber(event.target.value);
+
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -21,17 +27,32 @@ const App = () => {
     } else {
       const newPersons = [
         ...persons,
-        { name: newName, phoneNumber: newPhoneNumber },
+        { name: newName, number: newNumber, id: persons.length + 1 },
       ];
       setPersons(newPersons);
+      setFilteredPersons(newPersons);
     }
     setNewName("");
-    setNewPhoneNumber("");
+    setNewNumber("");
+  };
+
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchName(searchTerm);
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchTerm)
+    );
+    setFilteredPersons(filteredPersons);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:
+        <input type="text" value={searchName} onChange={handleSearch} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:
@@ -39,16 +60,16 @@ const App = () => {
         </div>
         <div>
           number:
-          <input value={newPhoneNumber} onChange={handleNewPhoneNumberChange} />
+          <input value={newNumber} onChange={handleNewNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((persons) => (
-        <p key={persons.name}>
-          {persons.name} {persons.phoneNumber}
+      {filteredPersons.map((persons) => (
+        <p key={persons.id}>
+          {persons.name} {persons.number}
         </p>
       ))}
     </div>
