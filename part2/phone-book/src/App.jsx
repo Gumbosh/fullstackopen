f0d4148935869d711bf0862d984e9691fb172ea1
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-15 10:19:05 +0100

    feat: Add solution for exercise 2.11

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DisplayNumbers from "./components/DisplayNumbers";
 import AddNewNumber from "./components/AddNewNumber";
 import Search from "./components/Search";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [searchName, setSearchName] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setFilteredPersons(response.data);
+    });
+  }, []);
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value);
