bf0460266f98e6204499066180a6fbc6e99fa3f3
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-21 12:07:33 +0100

    feat: Add solution for exercise 3.5

--- part3/phone-book-backend/index.js
+++ part3/phone-book-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3001;
+app.use(express.json());
 
 let persons = [
   {
@@ -25,6 +25,13 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const PORT = 3001;
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
@@ -51,6 +58,25 @@ app.delete("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
 app.get("/info", (req, res) => {
