efd346b0c77e6f17dbc97d3f7af518bafabf751f
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-27 10:54:52 +0100

    feat: Add solutions for exercises 3.16, 3.17, 3.18

--- part3/phone-book-backend/index.js
+++ part3/phone-book-backend/index.js
@@ -14,35 +14,35 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/api/persons", (req, res) => {
-  People.find({}).then((people) => {
-    res.json(people);
-  });
+app.get("/api/persons", (req, res, next) => {
+  People.find({})
+    .then((people) => {
+      res.json(people);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  People.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   People.findByIdAndDelete(req.params.id)
     .then((result) => {
       res.status(204).end();
     })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).send({ error: "Invalid id" });
-    });
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
@@ -56,17 +56,51 @@ app.post("/api/persons", (req, res) => {
     number: body.number,
   });
 
-  person.save().then((savedPeople) => {
-    res.json(savedPeople);
-  });
+  person
+    .save()
+    .then((savedPeople) => {
+      res.json(savedPeople);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`
-  );
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  People.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.get("/info", (req, res, next) => {
+  try {
+    res.send(
+      `<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "invalid id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 
