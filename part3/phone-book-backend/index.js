021ec4ab4c44d14d5cce9971f63c1952837c733a
GrzegorzWrzosek <grzegorz.j.wrzosek@gmail.com>
2024-03-27 09:26:48 +0100

    feat: Add solution for exercise 3.13

--- part3/phone-book-backend/index.js
+++ part3/phone-book-backend/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
 const app = express();
+const People = require("./models/people");
 
 app.use(express.json());
 app.use(cors());
@@ -13,38 +14,10 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const PORT = 3001;
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  People.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
@@ -102,6 +75,8 @@ app.get("/info", (req, res) => {
     <p>${new Date()}</p>`
   );
 });
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
